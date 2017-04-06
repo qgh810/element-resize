eb154bf84fd5b6d5b81c776e0075555b3d5dbc24
邱国辉 <530784837@qq.com>
2017-04-06 12:12:34 +0800

    添加Object标签

---
+++ dist/element-resize.js
@@ -0,0 +1,196 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define("elementResize", [], factory);
+	else if(typeof exports === 'object')
+		exports["elementResize"] = factory();
+	else
+		root["elementResize"] = factory();
+})(this, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 3);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+exports.checkNode = checkNode;
+function checkNode(el) {
+  HTMLElement.prototype.__defineGetter__('currentStyle', function () {
+    return this.ownerDocument.defaultView.getComputedStyle(this, null);
+  });
+  var result = el;
+  if (!result) {
+    return console.error('找不到当前节点', el);
+  }
+  if (typeof el === 'string') {
+    var domName = el;
+    result = document.querySelector(domName);
+    if (!result) {
+      return console.error('找不到当前节点', el);
+    }
+  } else if ((typeof el === 'undefined' ? 'undefined' : _typeof(el)) === 'object') {
+    if (!el.nodeName) {
+      return console.error('找不到当前节点', el);
+    }
+  }
+  return result;
+}
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _config = __webpack_require__(2);
+
+function showWarn(str) {
+  console.warn(str + ' 请参考相关文档: ' + _config.DOCUMENT_ADDR);
+}
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var DOCUMENT_ADDR = exports.DOCUMENT_ADDR = 'https://github.com/qgh810/element-resize';
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _check = __webpack_require__(0);
+
+var _log = __webpack_require__(1);
+
+var el = null;
+var callback = null;
+var objectNode = document.createElement('object');
+
+function elementResize(node, fn) {
+  el = (0, _check.checkNode)(node);
+  callback = fn;
+  el && init();
+}
+
+function init() {
+  setElStyle();
+  setAttribute();
+  appendObjectNode();
+}
+
+function setElStyle() {
+  var style = el.style;
+  var currentStyle = el.currentStyle;
+  if (currentStyle.position === 'static') {
+    style.position = 'relative';
+  }
+}
+
+function setAttribute() {
+  setObjectNodeStyle();
+  setObjectNodeType();
+}
+
+function setObjectNodeStyle() {
+  objectNode.setAttribute('style', '\n    position: absolute;\n    left: 1000px;\n    top: 1000px;\n    opacity: 0;\n    z-index: -1;\n    width: 100%;\n    height: 100%;\n  ');
+}
+
+function setObjectNodeType() {
+  objectNode.setAttribute('type', 'text/html');
+}
+
+function appendObjectNode() {
+  el.appendChild(objectNode);
+}
+
+module.exports = elementResize;
+
+/***/ })
+/******/ ]);
+});
--- src/main.js
+++ src/main.js
@@ -1,22 +1,54 @@
 import { checkNode } from './utils/check'
 import { showWarn } from './utils/log'
 
-function elementResize (el, callback) {
-  checkParams(el, callback) && init(el, callback)
+var el = null
+var callback = null
+var objectNode = document.createElement('object')
+
+function elementResize (node, fn) {
+  el = checkNode(node)
+  callback = fn
+  el && init()
 }
 
-function checkParams (el, callback) {
-  el = checkNode(el)
-  if (!el) return false
-  if (typeof callback !== 'function') {
-    showWarn('请传递回调函数')
-    return false
+function init () {
+  setElStyle()
+  setAttribute()
+  appendObjectNode()
+}
+
+function setElStyle () {
+  let style = el.style
+  let currentStyle = el.currentStyle
+  if (currentStyle.position === 'static') {
+    style.position = 'relative'
   }
-  return true
 }
 
-init (el, callback) {
-  console.log(el)
+function setAttribute () {
+  setObjectNodeStyle()
+  setObjectNodeType()
+}
+
+function setObjectNodeStyle () {
+  objectNode.setAttribute('style', `
+    position: absolute;
+    left: 1000px;
+    top: 1000px;
+    opacity: 0;
+    z-index: -1;
+    width: 100%;
+    height: 100%;
+  `)
+}
+
+function setObjectNodeType () {
+  objectNode.setAttribute('type', 'text/html')
+}
+
+
+function appendObjectNode () {
+  el.appendChild(objectNode)
 }
 
 
